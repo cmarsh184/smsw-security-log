043608c9bfd9784dd191e12e1d84ef9090857487
Callum Marshall <callum.marshall@smsw.co.uk>
2026-04-29 15:01:08 +0100

    secure dashboard with auth

---
+++ middleware.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const basicAuth = request.headers.get("authorization");
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(" ")[1];
+    const [user, password] = atob(authValue).split(":");
+
+    const validUser = process.env.DASHBOARD_USERNAME;
+    const validPassword = process.env.DASHBOARD_PASSWORD;
+
+    if (user === validUser && password === validPassword) {
+      return NextResponse.next();
+    }
+  }
+
+  return new NextResponse("Authentication required", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="SMSW Dashboard"',
+    },
+  });
+}
+
+export const config = {
+  matcher: ["/dashboard/:path*"],
+};
